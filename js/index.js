d878ee1401387f64de1dbf36776078d2a1a9c130
Sam Decker <samdecker386@gmail.com>
2020-02-11 23:25:14 -0700

    Completed MVP

--- js/index.js
+++ js/index.js
@@ -11,8 +11,12 @@ document.querySelectorAll(".nav-link").forEach(el => {
             el.style.color = "#212529";
         }, 500);
     }, false);
-});
 
+    // Prevent default as instructed
+    el.addEventListener("click", (event) => {
+        event.preventDefault();
+    });
+});
 
 // 2. Images change size when mouse enters
 document.querySelectorAll("img").forEach(el => {
@@ -35,10 +39,23 @@ header.style.zIndex = "1";
 document.querySelectorAll("p").forEach(el => {
     el.addEventListener("wheel", (el) => {
         el.target.style.fontSize = "1.8rem";
+
+        // Stop propogation #1
+        el.stopPropagation();
     });
 });
 
-// 5. Buttons at bottom of page change colors when clicked
+// 5. Paragraph background color changes when mouse leaves
+document.querySelectorAll("p").forEach(el => {
+    el.addEventListener("mouseout", (el) => {
+        el.target.style.background = "#f9f8fd";
+
+        // Stop propagation #2
+        el.stopPropagation();
+    });
+});
+
+// 6. Buttons at bottom of page change colors when clicked
 document.querySelectorAll(".btn").forEach(el => {
     el.addEventListener("click", (el) => {
         el.target.style.background = "#ffebcd";
@@ -46,34 +63,28 @@ document.querySelectorAll(".btn").forEach(el => {
     });
 });
 
-// 6. Double clicking content titles will make them bold
+// 7. Double clicking content titles will make them bold
 document.querySelectorAll(".logo-heading, h2, h4").forEach(el => {
     el.addEventListener("dblclick", (el) => {
         el.target.style.fontWeight = "bold";
+
+        // Stop propogation #3
+        el.stopPropagation();
     });
 });
 
-// 7. Header image changes when page is resized
+// 8. Header image changes when page is resized
 const funBus = document.querySelector("header img");
 window.addEventListener("resize", () => {
     funBus.src = "https://meetingsandconventionspei.com/wp-content/uploads/2015/02/Tour-Bus-Header.jpg"
 });
 
-// 8. Right click on mouse creates an alert message
+// 9. Right click on mouse creates an alert message
 document.addEventListener("auxclick", (el) => {
     alert("DON'T RIGHT CLICK THIS PAGE!");
 });
-const images = document.querySelectorAll("img");
 
-
-
-
-
-
-// images.forEach(event => {
-//     event.addEventListener("mouseenter", () => {
-//     images.style.transform = "scale(1.1)";
-//     images.style.transform = "transform 1s";
-// });
-
-// mouse over nav
+// 10. Loading or refreshing the page causes a prompt2020
+window.addEventListener('load', function(){
+    window.prompt('I just time-traveled. What year is it?');
+    });
